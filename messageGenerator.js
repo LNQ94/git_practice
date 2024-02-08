1b6c1f522dd900458dd64f8f7871a7d77831c24e
Cookie Queen <linh.nguyenquoc@gmx.de>
2024-02-08 20:19:59 +0100

    finish task

--- messageGenerator.js
+++ messageGenerator.js
@@ -1,1 +1,5 @@
-const messages = ['NO', 'Yes, definitely!', 'Ask me again after breakfast', 'Do not be stupid', 'Ask me again after a nap', 'Yo cannot ask me that', 'What does your heart tell you?', 'Why do you ask me?', 'That I cannot answer', 'You should do whatever you like', 'Haha :D', 'That is a good question']
+const messages = ['NO', 'Yes, definitely!', 'Ask me again after breakfast', 'Do not be stupid', 'Ask me again after a nap', 'Yo cannot ask me that', 'What does your heart tell you?', 'Why do you ask me?', 'That I cannot answer', 'You should do whatever you like', 'Haha :D', 'That is a good question'];
+
+let randomNumber = Math.floor(Math.random()*(messages.length+1));
+
+console.log(messages[randomNumber]);
