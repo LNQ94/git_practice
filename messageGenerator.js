980e11e9728511b05e4f0ff322d0f05cea157381
Cookie Queen <linhnguyenquoc@Linhs-iMac.box>
2024-02-08 19:58:22 +0100

    first commit

--- messageGenerator.js
+++ messageGenerator.js
@@ -1,1 +1,1 @@
-const messages = ['NO', 'Yes, definitely!', 'Ask me again after breakfast', 'Do not be stupid', 'Ask me again after a nap', 'Yo cannot ask me that', 'What does your heart tell you?', 'Why do you ask me?', 'That I cannot answer', 'You should do whatever you like', 'Haha :D']
+const messages = ['NO', 'Yes, definitely!', 'Ask me again after breakfast', 'Do not be stupid', 'Ask me again after a nap', 'Yo cannot ask me that', 'What does your heart tell you?', 'Why do you ask me?', 'That I cannot answer', 'You should do whatever you like', 'Haha :D', 'That is a good question']
